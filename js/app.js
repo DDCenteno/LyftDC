bcc741b387e058e802838d2543f00fe6b17e0dd4
DDCenteno <danielacentenoramos@gmail.com>
2017-12-31 20:13:35 -0500

    agregando segunda vista

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  setTimeout(function() { 
+  setTimeout(function() {
     window.location.href = 'views/pageOne.html';
   }, 1000);
 });
---
+++ js/appOne.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  $('#signUp').on('click', function() {
+    window.location.href = '../views/pageTwo.html';
+  });
+});
